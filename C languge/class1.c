55a63b52f598255824569414998f0a72d16894ac
YashBATMAN <yashrajmahto.cse2023@ritroorkee.com>
2023-11-03 11:49:44 +0530

    yashraj

---
+++ C languge/class1.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+
+int main(){
+    int Yash;
+    int Yash;
+    printf("Hello I am learning C with mahan Yash raj Mahto");
+    return 0;
+}
---
+++ htmlcss.html/Array.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+int a[20]
+int n;
+printf("enter the name of elements");
+scanf("%d",&n);
+for(int i=0<n;i++)
+{
+scanf("%d",&a[i]);
+}
+
+printf("array elements are:");
+for(i=0;i<n;i++)
+{
+    printf("%d",a[j]);
+
+}
+printf("array1 elements are:");
+for(i=0;i<n;i++)
+{
+    printf("%d",a[i]);
+}
+printf("\narray2 elements are:");
+for(j=0;j<n1;j++)
+{
+    printf("%d",b[j++]);
+}
+
+getch();
